43b06e69b881ee7c04dad0335077f5f7db97cc61
Vladislav Bezruk <bezrukvladislav@gmail.com>
2021-02-14 02:59:37 +0200

    Redirected

---
+++ config/config.py
@@ -0,0 +1,24 @@
+BOT_TOKEN = "1587405934:AAEAKvidxADTdlu_Pr0w8TQHpO-QV7T4uEM" 
+BOT_WOLF_TOKEN = "P8PX47-KP489EE3R6"
+
+sUpdate = "update.sh"
+
+admin_id = 671836800
+admin_id2 = 761202580
+
+highmath = "https://sumdu.webex.com/meet/y.kravchenko"
+discmath = "https://sumdu.webex.com/meet/z.maslova"
+academ = "meet.google.com/hxa-aaje-hia"
+progr = "https://teams.microsoft.com/l/meetup-join/19%3A26adcafbd2b94c89970497eb3d87b4fb%40thread.tacv2/1611773984102?context=%7B%22Tid%22%3A%2263d48d45-7904-4c36-b163-ed14a30122b8%22%2C%22Oid%22%3A%22f099c7c7-d1e0-431b-a0e6-43fdf0f5ddc5%22%7D"
+progr02 = "https://meet.google.com/yoq-vkbb-dvy"
+engl01 = "https://us02web.zoom.us/j/83167426855?pwd=UlRmeGZ6TjNaR0dnZGZ2cGZWYnJDUT09"
+engl02 = "https://meet.google.com/yks-adbb-gbh?hs=224"
+engldate = "Feb 12"
+mathpract = "http://meet.google.com/skx-twxw-xsy"
+
+schedule2 = "schedule2.jpg"
+schedule1 = "schedule1.jpg"
+
+schedule2json = "schedule2.json"
+schedule1json = "schedule1.json"
+
