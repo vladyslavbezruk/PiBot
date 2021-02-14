62f2887455703636d9e2400a361b57174222fd7f
Vladislav Bezruk <bezrukvladislav@gmail.com>
2021-02-14 03:03:58 +0200

    Redirected

--- sources/py/schedule.py
+++ sources/py/schedule.py
@@ -1,3 +1,13 @@
+'''
+Модуль для работы с расписанием.
+
+help_get_url() возвращает словарь с полями subject (имя предмета),
+teacher (имя преподователя), url, date, time для ближайшей пары
+или None, если такого не нашлось
+
+Все остальные функции - вспомогательные, не трогать!
+'''
+
 import json    #Работаем с json
 
 import codecs  #Читаем с учетом кодировки
@@ -41,14 +51,23 @@ def get_date_and_time(subj):
             date = element
             flag = True
         elif element[0].isdigit() and flag:
-            time = element.split(':')
-            time = int(time[0])*60 + int(time[1])
+            time = element
+            time_int = element.split(':')
+            time_int = int(time_int[0])*60 + int(time_int[1])
             break
-    return (date, time)
+    return (date, time_int, time)
 
+#Возвращает имя преподователя для строки DESCRIPTION
+def get_teacher_name(description):
+    return description.split('\\')[0]
+
+#Возвращает словарь с описанием ближайшего занятия или None
 def help_get_url():
+
+    #Есть ли сегодня занятия, по умолчанию - нет
     classes_today = False
-    
+
+    #Берем текущую дату и время в необходимом для сравнения варианте
     date = get_current_date()
     time = get_current_time()
     
@@ -57,12 +76,16 @@ def help_get_url():
     for subject in schedule['VCALENDAR'][0]['VEVENT']:
         #Сохраняем название предмета
         subj_name = subject['SUMMARY']
+        #Идем в функцию и забираем дату и время. 0 - дата, 1 - врямя для сравнения, 2 - время в виде строки
         date_time = get_date_and_time(subject)
+        #Если дата текущая и время меньше, чем начало пары
         if (date_time[0] == date and time < date_time[1]):
-            result = url_of_subject(subj_name)
+            url = url_of_subject(subj_name)
+            date = date_time[0]
             classes_today = True
+            teacher_name = get_teacher_name(subject['DESCRIPTION'])
             break
     if classes_today:
-        return result
+        return {'subject': subj_name, 'teacher': teacher_name, 'url': url, 'date': date, 'time': date_time[2]}
     else:
-        return 'Сегодня занятий нет!'
+        return None
