8d57016804d36d6d61e853eab952d066866f62bd
Ilya-Piskurov <piskryov@gmail.com>
2021-02-15 00:15:17 +0200

    BagFix way

--- sources/py/schedule.py
+++ sources/py/schedule.py
@@ -23,12 +23,11 @@ from datetime import datetime #Узнаем текущее время
 
 
 ''' Читаем с .json список предметов и словарь преподователь-ссылка '''
-# !!!!!!!!!!!!!!!!!!!!
-with codecs.open("../../resources/json/schedule1.json", encoding='utf-8') as schedule_file:
+with codecs.open("schedule1.json", encoding='utf-8') as schedule_file:
     #Сохраняем расписание в виде словаря Python
     schedule = json.loads(schedule_file.read())
 
-with codecs.open("../../resources/json/subjects.json", encoding='utf-8') as subjects_file:
+with codecs.open("subjects.json", encoding='utf-8') as subjects_file:
     #Сохранем пары преподаватель-ссылка в виде словаря Python
     subjects = json.loads(subjects_file.read())
 ''' Читаем с .json список предметов и словарь преподователь-ссылка '''
@@ -161,6 +160,6 @@ def help_tomorrow():
         elif classes_tomorrow:
             return result
         else:
-            return 'There are no lessons today.'
+            return 'There are no lessons tomorrow.'
 
 ''' ---------ОСНОВНЫЕ ФУНКЦИИ----------- '''
