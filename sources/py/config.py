8cae282ce84c98e2a9af277f867cf24e453a7ad4
Vladyslav Bezruk <bezrukvladislav@gmail.com>
2023-02-13 20:39:30 +0200

    Update config.py

--- sources/py/config.py
+++ sources/py/config.py
@@ -8,11 +8,10 @@ site = "https://schedule.sumdu.edu.ua/index/json?method=getSchedules&id_grp="
 
 clause = '·'
 
-BOT_TOKEN = "1587405934:AAEAKvidxADTdlu_Pr0w8TQHpO-QV7T4uEM" 
-BOT_WOLF_TOKEN = "P8PX47-KP489EE3R6"
+BOT_TOKEN = "" 
+BOT_WOLF_TOKEN = ""
 
 time_sleep = 60
-#time_sleep = 10
 
 time_before = 5
 
@@ -20,6 +19,3 @@ commands_page = 5
 
 admin_id  = 671836800
 admin_id2 = 761202580
-
-#api_id = 16415945
-#api_hash = 'ba1cf49ce4ad6033a0977a76578dc1d0'
