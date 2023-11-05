8312dd219f34a0c66bf119b2b81084ef446f0727
Vladyslav Bezruk <vladyslavbezruk@yahoo.com>
2023-11-05 22:06:14 +0200

    Update config.py

--- sources/py/config.py
+++ sources/py/config.py
@@ -3,8 +3,8 @@ site_groups = "https://schedule.sumdu.edu.ua/index/json?method=getGroups"
 
 clause = '·'
 
-BOT_TOKEN = "6619627596:AAHqmDJQOWjNIxq1zgeeAdVSVxRoCkxlaaU"
-BOT_WOLF_TOKEN = "P8PX47-KP489EE3R6"
+BOT_TOKEN = ""
+BOT_WOLF_TOKEN = ""
 
 time_sleep = 60
 
